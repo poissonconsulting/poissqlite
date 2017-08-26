7af40eb81c30fc9fec576029a175fb1815a7d2fa
Joe Thorley <joe@poissonconsulting.ca>
2017-08-26 16:14:41 -0700

    update README

--- document.R
+++ document.R
@@ -1,9 +1,2 @@
-library(devtools)
-library(knitr)
-library(pkgdown)
-library(poissqlite)
-
-document()
-knit("README.Rmd")
-build_site()
-knit("README.Rmd") # necessary because build_site messes with README.md
+devtools::document()
+pkgdown::build_site()
