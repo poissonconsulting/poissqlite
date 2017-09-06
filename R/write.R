d90d1134368c04f892beca5d07d6d70265b19498
Joe Thorley <joe@poissonconsulting.ca>
2017-09-06 14:11:03 -0700

    docs

--- R/write.R
+++ R/write.R
@@ -1,6 +1,13 @@
 #' Write Table
 #'
-#' Appends a data frame to an existing table in an SQLite database without row names.
+#' Appends a data frame to an existing table in an SQLite database
+#' without row names.
+#' More importantly it saves the time zone for POSIXct columns
+#' in the metadata table and converts the column to a character vector
+#' of format `YYYY-mm-dd HH:MM:SS`.
+#' \code{\link{ps_read_table}} looks up the time zone and
+#' converts the column back to a POSIXct vector with the original timezone.
+#'
 #'
 #' @param x The data frame.
 #' @param table_name A string of the name of the table.
