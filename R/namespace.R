0ac6a29ca1bda83bb258ebc94636367e8d1a9b32
Nadine Hussein <nadinehussein3@gmail.com>
2021-06-18 15:06:48 -0600

    updated namespace

--- R/namespace.R
+++ R/namespace.R
@@ -1,8 +1,7 @@
-#' @import blob DBI
+#' @import blob DBI chk
 #' @importFrom magrittr %<>% %>%
 #' @importFrom poisutils error ps_error ps_warning ps_message
 #' @importFrom yesno yesno
 #' @importFrom utils data
 #' @importFrom stats setNames
-#' @importFrom chk chk_string chk_unique chk_flag chk_scalar chk_named chk_list check_data check_names check_values chk_vector
 NULL
